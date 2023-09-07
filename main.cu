8dbf641001ac3d4d9ff18e95cb3c3e2038e68f89
Ubuntu <ubuntu@ip-172-31-34-127.eu-north-1.compute.internal>
2023-09-07 12:55:48 +0000

    Updated CMake file

--- main.cu
+++ main.cu
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/cudaimgproc.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
