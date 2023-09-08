f62bd2c748beecc659cc5b1903079896b065c416
simogiovannini <simone.giovannini1@stud.unifi.it>
2023-09-08 15:56:49 +0000

    Implemented copy data from device and atomicAdd

--- main.cu
+++ main.cu
@@ -13,7 +13,7 @@ void sequential_downscale(const Mat& image, int kernel_size);
 void cuda_downscale(const Mat& image, int kernel_size);
 void save_image(const Mat& image, const string& path);
 Vec3b compute_avg_pixel(const Mat& image, int kernel_size, int row_index, int col_index);
-__global__ void compute_avg_pixel_cuda(const uchar* img, const uchar*, int kernel_size, int img_dim);
+__global__ void compute_avg_pixel_cuda(const int* img, int* res, int kernel_size, int img_dim);
 
 int main()
 {
@@ -81,16 +81,17 @@ Vec3b compute_avg_pixel(const Mat& image, int kernel_size, int row_index, int co
 }
 
 void cuda_downscale(const Mat& image, int kernel_size) {
-
-    std::vector<uchar> tmp_array;
+    std::vector<int> tmp_array;
     tmp_array.assign(image.data, image.data + image.total()*image.channels());
 
-    uchar* gpu_img = nullptr;
-    cudaMalloc((void**)&gpu_img, image.total() * image.channels() * sizeof(uchar));
-    cudaMemcpy(gpu_img, tmp_array.data(), image.total() * image.channels() * sizeof(uchar), cudaMemcpyHostToDevice);
+    int* gpu_img = nullptr;
 
-    uchar* new_img = nullptr;
-    cudaMalloc((void**)&new_img, image.total() * image.channels() / (kernel_size * kernel_size) * sizeof(uchar));
+    cudaMalloc((void**)&gpu_img, image.total() * image.channels() * sizeof(int));
+    cudaMemcpy(gpu_img, tmp_array.data(), image.total() * image.channels() * sizeof(int), cudaMemcpyHostToDevice);
+
+    int* new_img = nullptr;
+    cudaMalloc((void**)&new_img, image.total() * image.channels() / (kernel_size * kernel_size) * sizeof(int));
+    cudaMemset(new_img, 0, image.total() * image.channels() / (kernel_size * kernel_size) * sizeof(int));
 
     int grid_dim = ceil((float) image.rows / 16);
 
@@ -100,19 +101,38 @@ void cuda_downscale(const Mat& image, int kernel_size) {
     compute_avg_pixel_cuda<<<grid, block>>>(gpu_img, new_img, kernel_size, image.rows);
     cudaDeviceSynchronize();
 
-    cv::Mat downscaled_img = Mat(image.rows / kernel_size, image.cols / kernel_size, CV_8UC3, &new_img);
+    cout << "cout" << image.total() * image.channels() / (kernel_size * kernel_size) << endl;
+
+    int* cuda_img = (int*)malloc(image.total() * image.channels() / (kernel_size * kernel_size) * sizeof(int));
+    cudaMemcpy(cuda_img, new_img, image.total() * image.channels() / (kernel_size * kernel_size) * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for(int i=0; i<12;i++){
+        cout << cuda_img[i] << endl;
+    }
+
+    cv::Mat downscaled_img = Mat(image.rows / kernel_size, image.cols / kernel_size, CV_8UC3, cuda_img);
     save_image(downscaled_img, "../cuda_result.jpg");
+
+    for(int i = 0; i < downscaled_img.rows; i += 1) {
+        for(int j = 0; j < downscaled_img.cols; j += 1) {
+            cout << downscaled_img.at<Vec3b>(Point(i, j)) << endl;
+        }
+    }
+
 }
 
-__global__ void compute_avg_pixel_cuda(const uchar* img, const uchar* res, int kernel_size, int img_dim) {
+__global__ void compute_avg_pixel_cuda(const int* img, int* res, int kernel_size, int img_dim) {
     int res_dim = img_dim / kernel_size;
     int i =  blockIdx.x * 16 + threadIdx.x;
     int j =  blockIdx.y * 16 + threadIdx.y;
     if(i < img_dim && j < img_dim) {
         int new_i = i / kernel_size;
         int new_j = j / kernel_size;
-        printf("%d %d %d -------> %d %d\n", i, j, threadIdx.z, new_i, new_j);
-        res[] =
+        // printf("%d %d %d -------> %d %d ------------ %d %d ------------ %d\n", i, j, threadIdx.z, new_i, new_j, new_j * res_dim * 3 + new_i * 3 + threadIdx.z, j * img_dim * 3 + i * 3 + threadIdx.z, res[new_j * res_dim * 3 + new_i * 3 + threadIdx.z]);
+        int pre = res[new_j * res_dim * 3 + new_i * 3 + threadIdx.z];
+        int add_val = (img[j * img_dim * 3 + i * 3 + threadIdx.z] / (kernel_size * kernel_size));
+        atomicAdd(&res[new_j * res_dim * 3 + new_i * 3 + threadIdx.z], add_val);
+        int post = res[new_j * res_dim * 3 + new_i * 3 + threadIdx.z];
+        printf("%d %d %d\n", pre, add_val, post);
     }
-
 }
